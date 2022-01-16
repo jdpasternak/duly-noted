22eeae8b4254430d9e66ddd2203ec3c0d1ab06da
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-15 14:38:18 -1000

    Add GET and POST routes for /notes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const PORT = 3001;
+
 app = express();
 
+app.use(express.json());
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
