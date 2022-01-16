a59105432a5cd2d55a242127d07ef24ddab915f0
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-15 15:49:58 -1000

    Add DELETE route and deleteNote function

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const notes = require("../../db/db.json");
-const { createNewNote, validateNote } = require("../../lib/notes");
+const { createNewNote, validateNote, deleteNote } = require("../../lib/notes");
 
 router.get("/notes", (req, res) => {
   res.json(notes);
@@ -12,6 +12,11 @@ router.post("/notes", (req, res) => {
     createNewNote(req.body, notes);
     res.json(req.body);
   }
+});
+
+router.delete("/notes/:id", (req, res) => {
+  deleteNote(req.params.id, notes);
+  res.send(`Note with ID ${req.params.id} deleted.`);
 });
 
 module.exports = router;
