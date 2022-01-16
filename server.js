066208bfb7a615441cdd5105c2d19f5b9faa4891
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-15 15:53:41 -1000

    Change PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app = express();
 
